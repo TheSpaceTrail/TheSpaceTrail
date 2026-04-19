c986887e5711ee3d6ab5404043ac7c312e17dd05
Christian Balkaran <cmrb.0009@gmail.com>
2026-04-19 01:08:31 -0400

    update parser to accept all new commands

--- src/parser.py
+++ src/parser.py
@@ -1,14 +1,23 @@
+import rich
 
-
-def sequence_parse(entry_sequence, return_sequence = None):
+def sequence_parse(json_file_pointer, entry_sequence, return_sequence = None):
     
     assert control_variables in globals or control_variables in locals
     assert data_variables in globals or data_variables in locals
 
+    json_sequence = json_file[entry_sequence]
+
+    current_sequence = entry_sequence
+
     item_index = 0
     while item_index < len(json_sequence):
         
-        item: str = json_sequence[item_index].strip()
+        item = json_sequence[item_index]
+
+        if type(item) == type(""):
+            item = item.strip()
+
+        choice = ""
 
         # variable substition
         if item.find("$") >= 0:
@@ -22,18 +31,84 @@ def sequence_parse(entry_sequence, return_sequence = None):
         control_char = item[0]
 
         if control_char == "!": # this item must be a command
+            
             command = item[1:]
-            exec_command(command, item_index)
+            
+            arguments = command.split(" ")
+            command_type = arguments[0]
+
+            match command_type:
+                case "store":
+
+                    variable_name = arguments[1]
+                    variable_data = arguments[2]
+                    data_variables[variable_name] = variable_data
+
+                case "jump_switch":
+                    
+                    switch_key = arguments[1]
+
+                    switch_dict = json_sequence[item_index + 1] # big problem if this isn't a dictionary
+                    assert type(switch_dict) == type({})
+
+                    switch_return = switch_dict[switch_key]
+
+                    sequence_parse(json_file_pointer, switch_return)
+
+                case "jump_if":
+
+                    operand1 = arguments[1]
+                    operand2 = arguments[2]
+                    operation = arguments[3]
+                    jump_sequence = arguments[4]
+
+                    if operation == ">":
+                        truth = operand1 > operand2
+                    
+                    elif operation == "<":
+                        truth = operand1 < operand2
+
+                    else:
+                        truth = operand1 == operand2
+
+                    sequence_parse(json_file_pointer, jump_sequence, current_sequence)
+
+                case "jump":
+                    
+                    jump_sequence = arguments[1]
+
+                    sequence_parse(json_file_pointer, jump_sequence, current_sequence)
+
+                case "end":
+                    sequence_parse(json_file_pointer, return_sequence, current_sequence)
 
         elif control_char == "?": # this item must be a question
+            
             question = item[1:]
-            exec_question(question, item_index)
+            
+            options = json_sequence[item_index + 1]
 
-        elif control_char == "*": # this item is arbitrary code execution
+            while True:
+
+                rich.print(question)
+                unchecked_choice = input()
+                
+                if unchecked_choice not in options:
+                    continue
+                else:
+                    choice = unchecked_choice
+                    break
+
+        elif control_char == "*": # this item is arbitrary code execution, probably don't use this
+            
             code = item[1:]
-            exec_code(code, item_index)
+            exec(code)
+            return
 
-        item_index += 1
+        else: # this item must be dialogue
+            rich.print(item)
 
+        item_index += 1 # move to the next item
+        continue
 
     
