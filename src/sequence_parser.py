c4f94a516d8796eb121c9916cb8b1835ffc741c6
Christian Balkaran <cmrb.0009@gmail.com>
2026-04-20 12:40:21 -0400

    make sequence_parser.py a while loop rather than recursive

--- src/sequence_parser.py
+++ src/sequence_parser.py
@@ -8,121 +8,148 @@ def sequence_parse(game_sequences, entry_sequence, data_variables, control_varia
     This means the game has been exited in someway when the head function returns.
     """
 
-    sequence_items = game_sequences[entry_sequence]
+    control_variables["game_end"] = False
+    next_sequence = entry_sequence
 
-    current_sequence = entry_sequence
+    while not control_variables["game_end"]:
 
-    item_index = 0
-    while item_index < len(sequence_items):
-        
-        item = sequence_items[item_index]
+        sequence_items = game_sequences[next_sequence]
 
-        print(item)
+        current_sequence = next_sequence
 
-        if type(item) == type(""):
-            item = item.strip()
-
-        choice = ""
-
-        # variable substition
-        if item.find("$") >= 0:
-            word_list: list[str] = item.split(" ")
-            for word in range(len(word_list)):
-                if word_list[word][0] == "$":
-                    word_list[word] = data_variables[word_list[word][1:].lower().strip()]
-
-            item = " ".join(word_list)
-
-        control_char = item[0]
-
-        if control_char == "!": # this item must be a command
+        item_index = 0
+        while item_index < len(sequence_items):
             
-            command = item[1:]
-            
-            arguments = command.split(" ")
-            command_type = arguments[0]
-            print(command_type, arguments)
+            item = sequence_items[item_index]
 
-            match command_type:
-                case "store":
+            print(item)
 
-                    variable_name = arguments[1].lower().strip()
-                    variable_data = arguments[2]
-                    data_variables[variable_name] = variable_data
-                    print(data_variables)
-                    print(data_variables[variable_name])
+            if type(item) == type(""):
+                item = item.strip()
 
-                case "jump_switch":
-                    
-                    switch_key = arguments[1].lower().strip()
+            choice = ""
 
-                    switch_dict = sequence_items[item_index + 1] # big problem if this isn't a dictionary
-                    item_index += 1 # so that it skips over the switch_dict on the next item 
-                    assert type(switch_dict) == type({})
+            # variable substition
+            if item.find("$") >= 0:
+                word_list: list[str] = item.split(" ")
+                for word in range(len(word_list)):
+                    if word_list[word][0] == "$":
+                        word_list[word] = data_variables[word_list[word][1:].lower().strip()]
 
-                    switch_return = switch_dict[switch_key]
+                item = " ".join(word_list)
 
-                    sequence_parse(json_file_pointer, switch_return, data_variables, control_variables)
+            control_char = item[0]
 
-                case "jump_if":
-
-                    operand1 = arguments[1]
-                    operand2 = arguments[2]
-                    operation = arguments[3]
-                    jump_sequence = arguments[4]
-
-                    if operation == ">":
-                        truth = operand1 > operand2
-                    
-                    elif operation == "<":
-                        truth = operand1 < operand2
-
-                    else:
-                        truth = operand1 == operand2
-
-                    sequence_parse(json_file_pointer, jump_sequence, data_variables, control_variables, current_sequence)
-
-                case "jump":
-                    
-                    jump_sequence = arguments[1]
-
-                    sequence_parse(json_file_pointer, jump_sequence, current_sequence)
-
-                case "end":
-                    sequence_parse(json_file_pointer, return_sequence, data_variables, control_variables, current_sequence)
-
-                case _: 
-                    print("No command was run")
-
-        elif control_char == "?": # this item must be a question
-            
-            question = item[1:]
-            
-            options = sequence_items[item_index + 1]
-            item_index += 1 # so that it skips over the options list on the next item 
-
-            while True:
-
-                rich.print(question)
-                unchecked_choice = input()
+            if control_char == "!": # this item must be a command
                 
-                if unchecked_choice not in options:
-                    continue
-                else:
-                    choice = unchecked_choice
-                    data_variables["choice"] = choice
-                    break
+                command = item[1:]
+                
+                arguments = command.split(" ")
+                command_type = arguments[0]
+                print(command_type, arguments)
 
-        elif control_char == "*": # this item is arbitrary code execution, probably don't use this
-            
-            code = item[1:]
-            exec(code)
-            return
+                match command_type:
+                    case "store":
 
-        else: # this item must be dialogue
-            rich.print(item)
+                        variable_name = arguments[1].lower().strip()
+                        variable_data = arguments[2]
+                        data_variables[variable_name] = variable_data
+                        print(data_variables)
+                        print(data_variables[variable_name])
 
-        item_index += 1 # move to the next item
-        continue
+                    case "jump_switch":
+                        
+                        switch_key = arguments[1].lower().strip()
+
+                        switch_dict = sequence_items[item_index + 1] # big problem if this isn't a dictionary
+                        item_index += 1 # so that it skips over the switch_dict on the next item 
+                        assert type(switch_dict) == type({})
+
+                        print(switch_dict)
+                        print(switch_key)
+
+                        switch_return = switch_dict[switch_key] # this the next sequence
+                        
+                        next_sequence = switch_return 
+                        prev_sequence = current_sequence
+                        break
+                        
+
+                    case "jump_if":
+
+                        operand1 = int(arguments[1])
+                        operand2 = int(arguments[2])
+                        operation = arguments[3]
+                        jump_sequence = arguments[4]
+
+                        if operation == ">":
+                            truth = operand1 > operand2
+                        
+                        elif operation == "<":
+                            truth = operand1 < operand2
+
+                        else:
+                            truth = operand1 == operand2
+
+                        if truth:
+                            next_sequence = jump_sequence
+                            prev_sequence = current_sequence
+                            break
+
+                    case "jump":
+                        
+                        jump_sequence = arguments[1]
+
+                        next_sequence = jump_sequence
+                        prev_sequence = current_sequence
+                        break
+
+                    case "end_sequence":
+                        next_sequence = prev_sequence
+                        break
+
+                    case "end_game":
+                        control_variables["game_end"] = True
+                        break
+
+                    case _: 
+                        print("Command control character used but no valid command was found.")
+
+            elif control_char == "?": # this item must be a question
+                
+                question = item[1:]
+                
+                options = sequence_items[item_index + 1]
+                item_index += 1 # so that it skips over the options list on the next item 
+
+                while True:
+
+                    rich.print(question)
+                    unchecked_choice = input()
+                    
+                    if unchecked_choice not in options:
+                        continue
+                    else:
+                        choice = unchecked_choice
+                        data_variables["choice"] = choice
+                        break
+
+            elif control_char == "*": # this item is arbitrary code execution, probably don't use this
+                
+                code = item[1:]
+                exec(code)
+                return
+
+            else: # this item must be dialogue
+                rich.print(item)
+
+            item_index += 1 # move to the next item
+            continue
+
+        if next_sequence != current_sequence:
+            break
+
+    print("Game ended successfully")
+
 
     
